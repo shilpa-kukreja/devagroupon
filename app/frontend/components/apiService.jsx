26712552d7dc73712bca1fd0a661d9ac270d449b
Shubham Shukla <shubshukla2332@gmail.com>
2025-11-27 17:58:07 +0530

    some changes

--- app/frontend/components/apiService.jsx
+++ app/frontend/components/apiService.jsx
@@ -1,5 +1,5 @@
 // services/apiService.js
-const API_BASE_URL =  'http://localhost:5000/api';
+const API_BASE_URL =  'https://devagroupon.onrender.com/api';
 
 // Helper function to handle API responses
 const handleResponse = async (response) => {
